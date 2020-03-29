bb351bdb126f7534e7c97931eb5bc0e10801b28b
ardato belay <aradatobelay@gmaill.com>
2020-03-29 20:38:27 +0300

    Updates

---
+++ precache-manifest.9324e3390239a678a57b115abf044ff1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e26516f0606581b60d30297d6395a0f",
+    "url": "/weather-app-react/index.html"
+  },
+  {
+    "revision": "b24ce93ea9270ca62fe3",
+    "url": "/weather-app-react/static/css/main.dc9aadc6.chunk.css"
+  },
+  {
+    "revision": "2bd183dd4e90502db9f8",
+    "url": "/weather-app-react/static/js/2.e72889b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app-react/static/js/2.e72889b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b24ce93ea9270ca62fe3",
+    "url": "/weather-app-react/static/js/main.27e0b32f.chunk.js"
+  },
+  {
+    "revision": "c2e0e510477e615d13ea",
+    "url": "/weather-app-react/static/js/runtime-main.dc32c2fd.js"
+  }
+]);
